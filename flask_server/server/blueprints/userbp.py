2dc7b0aa8fd143ce4f693d90e74309f4824670a3
Nate <nrhamm17@gmail.com>
2019-04-16 20:25:39 -0500

    Added endpoint for querying users

--- flask_server/server/blueprints/userbp.py
+++ flask_server/server/blueprints/userbp.py
@@ -1,21 +1,47 @@
 import json
-from flask import Blueprint, request,make_response
+from flask import Blueprint, request, jsonify
 
 from server.loggers.serverlogger import request_log
-from server.db.usersession import UserSession
+from server.cachemanager import poolman
+from server.auth import require_auth
+from server.db import db_session
+from server.models import User
 
-_user_session = UserSession()
 
 bp = Blueprint('user', __name__, url_prefix='/user')
 
+
 @bp.route('/')
+@require_auth
 def request_userlist():
-    pass
+    """Gets mapping of users -> usernames for online users
+    """
+    with poolman as r:
+        user_list = {user_id: r.hget(
+            f'user:{user_id}', 'username') for user_id in r.sscan_iter('user:online')}
+    return jsonify(usersOnline=user_list)
+
 
 @bp.route('/<int:user_id>')
+@require_auth
 def request_username(user_id):
-    pass
+    """Queries user by ID
+    """
+    with poolman as r:
+        if r.exists(f'user:{user_id}'):
+            return jsonify(user={user_id: r.hget(f'user:{user_id}', 'username')})
+        else:
+            queried_user = db_session.query(User).filter(
+                User.id == user_id).one_or_none()
+            if queried_user:
+                return jsonify(user={user_id: queried_user.username})
+            else:
+                return jsonify(error="No user found")
+
 
 @bp.route('/<string:username>')
 def request_user_id(username):
+    """Queries User by username
+    """
+    # TODO rework redis so that i can efficiently query users by username
     pass
