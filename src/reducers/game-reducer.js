62d3bca7e2cc168482f02c018cfc615ccbf5f287
nate <nrhamm17@gmail.com>
2019-07-30 20:57:54 -0500

    Adapted sample code from @summerdeehan

--- src/reducers/game-reducer.js
+++ src/reducers/game-reducer.js
@@ -1,8 +1,17 @@
 // import MovementActions, { MOVE_PLAYER } from '../constants/action-types/movement-types'
 
+
+
+
+
+
+
 const initialGameState = {
   gameObjects: {
-    0: {}
+    0:{},
+    1:{},
+    2:{},
+    3:{},
   }
 };
 export default function gameReducer(state = initialGameState, action) {
