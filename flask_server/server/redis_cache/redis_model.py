3df7dab3737617f1a4953c100557bf064b06fbda
Nate <nrhamm17@gmail.com>
2019-10-01 13:35:42 -0500

    missing file

--- flask_server/server/redis_cache/redis_model.py
+++ flask_server/server/redis_cache/redis_model.py
@@ -1,37 +1,8 @@
 import redis
 
 
-class RedisObject:
-    REDIS = None
-
-    @classmethod
-    def init_redis(cls, *args, **kwargs):
-        cls.REDIS = redis.Redis(*args, **kwargs)
-
-
-class RedisModel(RedisObject):
-    def __init__(self, *args, **kwargs):
-
-        super().__init__(*args, **kwargs)
-
-
-class RedisType(RedisObject):
+class RedisModel:
     REDIS = None
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-    def get_entry(self):
-        pass
-
-    def set_entry(self):
-        pass
-
-    def clean(self):
-        pass
-
-    def make_entry(self):
-        pass
-
-    def delete_entry(self):
-        pass
